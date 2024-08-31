0a38a7df6918dcfcf28cd2aee0c2d2d5849a4a07
ajay835880 <ajaykushwaha68968@gmail.com>
2024-09-01 01:58:39 +0530

    get user details

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
+require("dotenv").config
 
 app.use(express.json());
 
 app.use("/api/get", (req,res)=>{
-    res.json({message: "This is Ajay Kushwaha webserver"})
+    res.json({message: "This is Ajay Kushwaha webserver"
+
+    })
 });   
+ 
+app.use("/api/get/user/detail",(req,res)=>{
+    res.json({user:{
+        name:"Ajay Kushwaha",
+        age: 24,
+        contact: 857458213
+    },env:process.env.NAME
+  });
+})
 
 
-const PORT =  7800;
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
